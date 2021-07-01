8101df3f41c4589bc3f73cfce9a95cf3496e8e42
auralshin <anshulspartan141@gmail.com>
2021-07-01 19:10:33 +0530

    minor changes

--- src/routes/post.js
+++ src/routes/post.js
@@ -5,16 +5,16 @@ const { requireSignin } = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post("/createPost" , postController.createPost);  //New
-router.get("/getPostByTwitterId/:tweetId",postController.getPostByTweetId); //New
-router.get("/getAllPostsModerator",postController.getAllPostsModerator); //New
-router.get("/getAllPostsUser/:userId",postController.getAllPostsUser); //New
-router.get("/dashboardPostPending",postController.getAllPendingPost); // New
+router.post("/createPost" , postController.createPost);  //New t
+router.get("/getPostByTwitterId/:tweetId",postController.getPostByTweetId); //New t
+router.get("/getAllPostsModerator",postController.getAllPostsModerator); //New t
+router.get("/getAllPostsUser/:userId",postController.getAllPostsUser); //New t
+router.get("/dashboardPostPending",postController.getAllPendingPost); // New t
 router.get("/getHandleReputation",postController.getHandleReputation); //New
-router.get("/getReputationPointsOfModerator/:moderatorId",postController.getReputationPointsOfModerator);
+router.get("/getReputationPointsOfModerator/:moderatorId",postController.getReputationPointsOfModerator); //New
 router.post("/createPostTweet" , postController.createTweetPost);
 router.get("/getUser",requireSignin,postController.getUser);
-router.post("/giveRating",requireSignin,postController.giveRating);
+router.post("/giveRating",requireSignin,postController.giveRating);//New
 
 
 
