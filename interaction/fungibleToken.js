85085c965a88bbc5ccc4aa37a8671818e8d35d0e
Gulshan Vasnani <gulshanslvasnani@gmail.com>
2021-07-10 12:07:05 +0530

    Added interact for transferFrom

--- interaction/fungibleToken.js
+++ interaction/fungibleToken.js
@@ -3,7 +3,7 @@ const { BN, Long, bytes, units } = require('@zilliqa-js/util');
 const { toBech32Address, fromBech32Address } = require('@zilliqa-js/crypto');
 
 //You can set the value of the following variables according to your liking
-let contractAddress = "0x4e38fa964533e2c23e7247112e28f88161a45bb8"; //
+let contractAddress = "0x4e38fa964533e2c23e7247112e28f88161a45bb8"; // toBechAddress : zil1fcu049j9x03vy0njgugju28cs9s6gkach57zcc
 let privateKey = "3ddf47aff62044916ea19f62d1bc480d52ccbf8cf35cd8dd8aa0039dade5cced";
 const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
                               
@@ -95,6 +95,71 @@ async function transfer(recipientAddress, sendingAmount) {
   }
 
 }
+
+
+async function transferFrom(fromAddress, toAddress, sendingAmount) {
+  try {
+    recipientAddress = fromBech32Address(recipientAddress); //converting to ByStr20 format
+    //  console.log('tobech address: ', toBech32Address(nonBechAddress));
+    //  const convertedBechAddress = toBech32Address(nonBechAddress);
+    // console.log('convertedBechAddress : ', convertedBechAddress);
+
+    //  console.log('type convertedBechAddress ', typeof convertedBechAddress);
+    console.log('type recipientAddress ', typeof recipientAddress);
+
+    // Get Minimum Gas Price from blockchain
+    const minGasPrice = await zilliqa.blockchain.getMinimumGasPrice();
+
+    const myGasPrice = units.toQa('2000', units.Units.Li);
+
+    const isGasSufficient = myGasPrice.gte(new BN(minGasPrice.result));
+    console.log('isGasSufficient :  ', isGasSufficient);
+    const callTx = await contract.callWithoutConfirm(
+      'TransferFrom',
+      [
+        {
+          vname: 'from',
+          type: 'ByStr20',
+          value: fromAddress,
+        },
+        {
+          vname: 'to',
+          type: 'ByStr20',
+          value: toAddress,
+        },
+        {
+          vname: 'amount',
+          type: 'Uint128',
+          value: sendingAmount,
+        }
+      ],
+      {
+        // amount, gasPrice and gasLimit must be explicitly provided
+        version: VERSION,
+        amount: new BN(0),
+        gasPrice: myGasPrice,
+        gasLimit: Long.fromNumber(25000),
+      },
+      false
+    );
+    // console.log("transfer : ", JSON.stringify(callTx));
+    // return callTx;
+    console.log(callTx.bytes);
+
+    console.log(`The transaction id is:`, callTx.id);
+    console.log(`Waiting transaction be confirmed`);
+    const confirmedTxn = await callTx.confirm(callTx.id);
+
+    console.log(`The transaction status is:`);
+    console.log(confirmedTxn.receipt);
+  } catch (err) {
+    console.log(err.message);
+    return err.message;
+  }
+
+}
+
+
 
 exports.getState = getState;
 exports.transfer = transfer;
